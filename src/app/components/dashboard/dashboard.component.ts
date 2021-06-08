906732d3626f00b225abbe051f6e87e9542d24df
Laura Schell <laurabelenschell@gmail.com>
2021-06-08 14:11:00 -0300

    finished

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/models/task';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  taskList: Task[] = [];
+  taskName = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addTask() {
+    const task: Task = { name: this.taskName, status: false };
+
+    this.taskList.push(task);
+
+    this.taskName = '';
+  }
+
+  removeTask(index: number): void {
+    this.taskList.splice(index, 1);
+  }
+
+  updateTask(task: Task, index: number): void {
+    this.taskList[index].status = !task.status
   }
 
 }
